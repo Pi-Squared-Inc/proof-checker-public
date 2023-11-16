ef335191eceba3c4652fde4c49ed69d414557001
devops <devops@runtimeverification.com>
2023-11-16 02:30:41 +0000

    Publish Public Files

--- cpp/src/lib.hpp
+++ cpp/src/lib.hpp
@@ -876,7 +876,7 @@ struct Pattern {
     // Axioms
     // Prop1: phi0 => (phi1 => phi0)
     // Prop2: (phi0 => (phi1 => phi2)) => ((phi0 => phi1) => (phi0 => phi2))
-    // Prop3: (~(~phi0 => phi0)
+    // Prop3: ((~(~phi0)) => phi0)
     auto prop1 = implies(phi0.clone(), implies(phi1.clone(), phi0.clone()));
     auto prop2 =
         implies(implies(phi0.clone(), implies(phi1.clone(), phi2.clone())),
