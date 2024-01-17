fadac4ee797a5d7c1db3639d5d676366f12d1161
devops <devops@runtimeverification.com>
2024-01-17 13:27:20 +0000

    Publish Public Files

--- risc0/bench.sh
+++ risc0/bench.sh
@@ -9,17 +9,6 @@ fi
 output_file="$1"
 proofs_path="../proofs/translated"
 
-
-print_output() {
-    echo "$2" >> "$output_file"
-    echo "$1" | grep -E "Total cycles" >> "$output_file"
-    echo "$1" | grep -E "Ran in " >> "$output_file"
-    echo "$1" | grep -E "Proved in " >> "$output_file"
-    echo "$1" | grep -E "Verified in " >> "$output_file"
-    echo "$1" | grep -E "Running execution \+ ZK certficate generation \+ verification" >> "$output_file"
-    echo "-----------------------------------------------------------------------" >> "$output_file"
-}
-
 direct-example() {
     (
         cd tests/direct-implementation || exit
@@ -46,33 +35,24 @@ batch-transfer-csl() {
     direct-example "transfer5000"
 }
 
-pi2-example() {
-    echo "Running $1"
-    gamma="$proofs_path/$1/$1.ml-gamma"
-    claim="$proofs_path/$1/$1.ml-claim"
-    proof="$proofs_path/$1/$1.ml-proof"
-    output=$(cargo run --release --bin host "$gamma" "$claim" "$proof")
-    print_output "$output" "$1"
-}
-
 perceptron-goal() {
-    pi2-example "perceptron-goal"
+    ./check_with_risc0.sh --stats -o "$output_file" "$proofs_path/perceptron-goal"
 }
 
 svm-goal() {
-    pi2-example "svm5-goal"
+    ./check_with_risc0.sh --stats -o "$output_file" "$proofs_path/svm5-goal"
 }
 
 batch-transfer() {
-    pi2-example "transfer-batch-1k-goal"
+    ./check_with_risc0.sh --stats -o "$output_file" "$proofs_path/transfer-batch-1k-goal"
 }
 
 transfer-goal() {
-    pi2-example "transfer-simple-compressed-goal"
+    ./check_with_risc0.sh --stats -o "$output_file" "$proofs_path/transfer-simple-compressed-goal"
 }
 
 impreflex() {
-    pi2-example "impreflex-compressed-goal"
+    ./check_with_risc0.sh --stats -o "$output_file" "$proofs_path/impreflex-compressed-goal"
 }
 
 clean_up() {
---
+++ risc0/check_with_risc0.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+# Usage: check_with_risc0.sh [options] <input directory>
+#
+# Options:
+#  -a, --available            Print if the program is available or not
+#  -h, --help                 Print this help message
+#  -o, --output FILENAME      Output file
+#  -s, --stats                Print statistics about the input
+
+BASH_SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+BUILD_DIR="$BASH_SOURCE_DIR/../.build"
+
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        -a|--available)
+            if [ -f "$BUILD_DIR/target/release/host" ]; then
+                echo "1"
+            else
+                echo "0"
+            fi
+            exit 0
+            ;;
+        -h|--help)
+            echo "Usage: check_with_risc0.sh [options] <input directory>"
+            echo ""
+            echo "Options:"
+            echo " -a, --available         Print if the program is available or not"
+            echo " -h, --help              Print this help message"
+            echo " -o, --output FILENAME   Save the output to a file"
+            echo " -s, --stats             Print statistics about the execution"
+            exit 0
+            ;;
+        -o|--output)
+            output_file="$2"
+            shift
+            shift
+            ;;
+        -s|--stats)
+            stats=true
+            shift
+            ;;
+        *)
+            input="$1"
+            shift
+            ;;
+    esac
+done
+
+if [ -z "$input" ]; then
+    echo "Usage: check_with_risc0.sh [options] <input directory>"
+    exit 1
+fi
+
+# Check if the input directory exists
+if [ ! -d "$input" ]; then
+    echo "Input directory does not exist"
+    exit 1
+fi
+
+# Check if the input directory contains the input files necessaries for RISC0
+input_filename=$(basename "$input")
+if [ ! -f "$input/$input_filename.ml-gamma" ]; then
+    echo "Input directory does not contain a $input_filename.ml-gamma file"
+    exit 1
+fi
+
+if [ ! -f "$input/$input_filename.ml-claim" ]; then
+    echo "Input directory does not contain a $input_filename.ml-claim file"
+    exit 1
+fi
+
+if [ ! -f "$input/$input_filename.ml-proof" ]; then
+    echo "Input directory does not contain a $input_filename.ml-proof file"
+    exit 1
+fi
+
+# Setting the inputs variable
+gamma="$input/$input_filename.ml-gamma"
+claim="$input/$input_filename.ml-claim"
+proof="$input/$input_filename.ml-proof"
+
+# If $output_file is not set, then we will print the output to stdout
+if [ -z "$output_file" ]; then
+    output_file="/dev/stdout"
+fi
+
+# Execute the RISC0 Proof-Checker with the input files
+if [ "$stats" ]; then
+   output=$(cargo run --release --bin host "$gamma" "$claim" "$proof")
+   {
+        echo "$input_filename"
+        echo "$output" | grep -E "Total cycles"
+        echo "$output" | grep -E "Ran in "
+        echo "$output" | grep -E "Proved in "
+        echo "$output" | grep -E "Verified in "
+        echo "$output" | grep -E "Running execution \+ ZK certficate generation \+ verification"
+        echo "-----------------------------------------------------------------------"
+   } >> "$output_file"
+else
+    cargo run --release --bin host "$gamma" "$claim" "$proof" >> /dev/null
+fi
+
+exit 0;
